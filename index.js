b18c708516a433a4ecec6865d695266aa817500b
anaskaciw <argawicak@gmail.com>
2020-12-18 15:33:02 +0700

    saya tambahkan gitinore

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', function(req, res){
+  res.send('Hello My name is YOUR_NAME');
+});
+
+app.post('/', function(req, res){
+  res.send('Hello My name is YOUR_NAME but it from post');
+});
+
+app.put('/', function(req, res){
+  res.send('Hello My name is YOUR_NAME but it from put');
+});
+
+app.patch('/', function(req, res){
+  res.send('Hello My name is YOUR_NAME but it from patch');
+});
+
+app.delete('/', function(req, res){
+  res.send('Hello My name is YOUR_NAME but it from delete');
+});
+
+app.listen(port, function(){
+  console.log('Hello my apps is running on port '+port);
+});
